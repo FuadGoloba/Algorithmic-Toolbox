31b207befa6161eda2cbc60eb59b33d20e69b9ba
FuadGoloba <fuadgoloba@gmail.com>
2022-07-28 07:42:37 +0100

    Greedy Algorithms

---
+++ 3. Greedy Algorithms/1. Money Change/money_change.py
@@ -0,0 +1,33 @@
+# Money Change Problem
+# Compute the minimum number of coins needed to change the given value into coins with denominations 1, 5, and 10.
+    # Input: An integer money.
+    # Output: The minimum number of coins with denominations 1, 5, and 10 that changes money.
+
+# Example :
+            # Input: 28
+            # Output: 6
+            # Solution : 28 = 10 + 10 + 5 + 1 + 1 + 1.
+
+def money_change(money):
+    assert 0 <= money <= 10 ** 3
+
+    num_of_coins = 0
+    while money > 0:
+        if money >= 10:
+            money -= 10
+        elif money >= 5:
+            money -= 5
+        elif money >= 1:
+            money -= 1
+        num_of_coins += 1
+    return num_of_coins
+
+def money_change2(money):
+    assert 0 <= money <= 10 ** 3
+
+    return (money // 10 + (money % 10) // 5 + (money % 5))
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(money_change(input_n))
